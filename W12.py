a2e8182cb299689f2cc78b819ac40821af1a89db
Ayaan <M.Hassan2477@student.leedsbeckett.ac.uk>
2025-10-13 10:11:46 +0100

    Change

--- W12.py
+++ W12.py
@@ -3,4 +3,4 @@ if __name__ == "__main__":
     kgs = 76
     stones = kgs / 6.34
 
-    print(f"{kgs:.1f}Kg is equivalent to {stones:.1f} Stone get back to the gym.")
+    print(f"{kgs:.1f}Kg is equivalent to {stones:.1f} Stone get back to the gym and lose that weight.")
